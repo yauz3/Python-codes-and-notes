45313749f027c78ce88a7882077bbc8038bde605
yauz3 <60094745+yauz3@users.noreply.github.com>
2022-07-10 01:34:41 +0300

    Add files via upload

---
+++ DB basics.py
@@ -0,0 +1,7 @@
+"""
+https://www.guru99.com/dbms-tutorial.html
+https://www.studocu.com/en-gb/document/durham-university/computer-systems/database-note-lecture-notes-1-5/13431903
+https://beginnersbook.com/2015/04/dbms-tutorial/
+https://www.w3schools.com/sql/sql_intro.asp
+
+"""
