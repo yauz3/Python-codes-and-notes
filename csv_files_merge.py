65894b25675d7e50cce343e611ebf6c7c5e04936
Sadettin Yavuz Ugurlu <60094745+yauz3@users.noreply.github.com>
2022-08-04 12:51:05 +0100

    Update csv_files_merge.py

--- csv_files_merge.py
+++ csv_files_merge.py
@@ -8,7 +8,7 @@ def merge_one_after_the_other(csv_file_1,csv_file_2): # alt alta birleştirmek (
     result = pd.concat([data_1, data_2])
     result.to_csv("merged_csv_file.csv")
 
-def merge_side_by_side():  # yan yana birleştirmek (TR)
+def merge_side_by_side(csv_file_1,csv_file_2):  # yan yana birleştirmek (TR)
     data_1 = pd.read_csv(f"{csv_file_1}", index_col=0)
     data_2 = pd.read_csv(f"{csv_file_2}", index_col=0)
     result = pd.concat([data_1, data_2], axis=1)
